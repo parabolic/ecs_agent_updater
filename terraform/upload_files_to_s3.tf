7e95d996eba7d10b5f54c414eaccaf4b7dfac4ed
parabolic <nvelkovski@gmail.com>
2018-02-20 20:38:11 +0100

    remove terraform zip resource because it changed the file permissions, remove the unused code

--- terraform/upload_files_to_s3.tf
+++ terraform/upload_files_to_s3.tf
@@ -1,20 +1,9 @@
-# This template zips and uploads the lambda code to s3.
-
-data "archive_file" "zip_the_binary" {
-  type        = "zip"
-  source_file = "../bin/${local.application_name}"
-  output_path = "../bin/${local.application_name}.zip"
-}
+# This template uploads the lambda code to s3.
 
 resource "aws_s3_bucket_object" "upload_zipped_function_to_s3" {
-  /* # Remove the "/tmp/" part of the string so when it gets uploaded to s3 it doesn't */  /* # go to a folder /tmp/ and instead goes to the base of the bucket */
+  key = "${local.binary_file_name}.zip"
 
-  /* key = "${replace("${data.archive_file.lambda_function_cloudwatch_to_slack.output_path}", "/[[:punct:]]tmp[[:punct:]]/", "")}" */
-
-  key = "${local.application_name}.zip"
-
-  bucket     = "${aws_s3_bucket.go_lambda_function.id}"
-  source     = "${data.archive_file.zip_the_binary.output_path}"
-  etag       = "${md5(file("${data.archive_file.zip_the_binary.output_path}"))}"
-  depends_on = ["data.archive_file.zip_the_binary"]
+  bucket = "${aws_s3_bucket.go_lambda_function.id}"
+  source = "${local.binary_file_path}.zip"
+  etag   = "${md5(file("${local.binary_file_path}.zip"))}"
 }
