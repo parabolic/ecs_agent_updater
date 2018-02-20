eddaa7d62bcfec978c2a8dbcd4551fa12414eecf
parabolic <nvelkovski@gmail.com>
2018-02-20 20:35:06 +0100

    add the target for lambda

--- terraform/cloudwatch_event.tf
+++ terraform/cloudwatch_event.tf
@@ -3,3 +3,9 @@ resource "aws_cloudwatch_event_rule" "ecs_agent_updater" {
   description         = "Run the ${local.application_name} every monday at 11 am UTC"
   schedule_expression = "cron(0 11 ? * MON *)"
 }
+
+resource "aws_cloudwatch_event_target" "lambda" {
+  rule      = "${aws_cloudwatch_event_rule.ecs_agent_updater.name}"
+  target_id = "TriggerLambda"
+  arn       = "${aws_lambda_function.ecs_agent_updater.arn}"
+}
