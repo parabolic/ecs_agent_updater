a436600f08b1d826903c5500f4e00583e82200d3
parabolic <nvelkovski@gmail.com>
2018-02-20 20:35:49 +0100

    increase the timeout and shuffle the lines

--- terraform/lambda_function.tf
+++ terraform/lambda_function.tf
@@ -1,19 +1,20 @@
 resource "aws_lambda_function" "ecs_agent_updater" {
-  function_name    = "${local.environment}-${local.application_name}"
+  runtime          = "go1.x"
   description      = "Lambda function for updating outdated ecs agents."
-  s3_key           = "${replace("${data.archive_file.zip_the_binary.output_path}", "../bin/", "")}"
+  timeout          = "10"
+  depends_on       = ["aws_s3_bucket_object.upload_zipped_function_to_s3"]
+  function_name    = "${local.environment}-${local.application_name}"
+  s3_key           = "${local.binary_file_name}.zip"
   s3_bucket        = "${aws_s3_bucket.go_lambda_function.id}"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
-  handler          = "lambda_function.lambda_handler"
-  source_code_hash = "${base64sha256(file("${data.archive_file.zip_the_binary.output_path}"))}"
-  runtime          = "go1.x"
+  handler          = "${local.binary_file_name}"
+  source_code_hash = "${base64sha256(file("${local.binary_file_path}"))}"
   kms_key_arn      = "${aws_kms_key.kms_for_lambda.arn}"
-  depends_on       = ["aws_s3_bucket_object.upload_zipped_function_to_s3"]
 
   environment {
     variables = {
       SLACK_WEBHOOK_ENDPOINT = "${var.slack_webhook_endpoint}"
-      UPDATE_ECS_AGENT       = "true"
+      UPDATE_ECS_AGENT       = "${var.update_ecs_agent}"
     }
   }
 
