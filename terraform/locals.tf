18db6669d36b734d5c58c28e6daddb9ea2e6c5b1
parabolic <nvelkovski@gmail.com>
2018-02-20 20:37:27 +0100

    shuffle the lines and add binary variable

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,6 +1,8 @@
 locals = {
-  environment               = "${terraform.workspace}"
   application_name          = "ecs_agent_updater"
-  application_name_alphanum = "${replace(local.application_name,"_","-")}"
+  binary_file_name          = "main"
+  binary_file_path          = "../bin/${local.binary_file_name}"
   region                    = "eu-west-1"
+  environment               = "${terraform.workspace}"
+  application_name_alphanum = "${replace(local.application_name,"_","-")}"
 }
