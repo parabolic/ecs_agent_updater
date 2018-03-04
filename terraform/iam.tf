4f6053e766b7a6325b6148ca0e0a74583d779ea0
parabolic <nvelkovski@gmail.com>
2018-03-04 20:51:48 +0100

    fix the rendering of the template

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,7 +1,15 @@
+data "template_file" "lambda_kms_policy" {
+  template = "${file("${path.module}/policies/lambda_kms_policy.json")}"
+
+  vars {
+    kms_for_lambda_arn = "${aws_kms_key.kms_for_lambda.arn}"
+  }
+}
+
 resource "aws_iam_role_policy" "lambda_kms_policy" {
   name   = "${local.environment}_${local.application_name}_lambda_policy"
   role   = "${aws_iam_role.iam_for_lambda.id}"
-  policy = "${file("${path.module}/policies/lambda_kms_policy.json")}"
+  policy = "${data.template_file.lambda_kms_policy.rendered}"
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
