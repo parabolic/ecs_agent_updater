cf8a6ffc9620b91cff6a763b6f75889985dba590
parabolic <nvelkovski@gmail.com>
2018-03-04 20:04:29 +0100

    remove secrets from the deploy script

--- terraform_deploy.sh
+++ terraform_deploy.sh
@@ -11,4 +11,4 @@ cd ${WORKING_DIRECTORY}
 ls .terraform | grep -v plugins | awk '{print ".terraform/" $1}' | xargs rm -rf
 terraform init -upgrade
 terraform workspace select ${WORKSPACE}
-terraform apply -var-file=${SECRETS_FILE} "$@"
+terraform apply "$@"
