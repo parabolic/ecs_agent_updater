020485cc40ad3035046accd5f709fe736412a6ca
parabolic <nvelkovski@gmail.com>
2018-03-04 20:30:53 +0100

    add the schedule expression

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,8 +1,10 @@
 locals = {
-  application_name          = "ecs_agent_updater"
-  binary_file_name          = "main"
-  binary_file_path          = "../bin/${local.binary_file_name}"
-  region                    = "eu-west-1"
-  environment               = "${terraform.workspace}"
-  application_name_alphanum = "${replace(local.application_name,"_","-")}"
+  // Run every monday at 11:00
+  cloudwatch_schedule_expression = "cron(0 11 ? * MON *)"
+  application_name               = "ecs_agent_updater"
+  binary_file_name               = "main"
+  binary_file_path               = "../bin/${local.binary_file_name}"
+  region                         = "eu-west-1"
+  environment                    = "${terraform.workspace}"
+  application_name_alphanum      = "${replace(local.application_name,"_","-")}"
 }
