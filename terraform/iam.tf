2d00faff1341b2890a7fc039dae1710214123a76
parabolic <nvelkovski@gmail.com>
2018-03-04 20:05:37 +0100

    move the policy into e separate file

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,38 +1,7 @@
 resource "aws_iam_role_policy" "lambda_kms_policy" {
-  name = "${local.environment}_${local.application_name}_lambda_policy"
-  role = "${aws_iam_role.iam_for_lambda.id}"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "Stmt1443036478000",
-      "Effect": "Allow",
-      "Action": [
-        "kms:decrypt"
-      ],
-      "Resource": [
-        "${aws_kms_key.kms_for_lambda.arn}"
-      ]
-    },
-    {
-      "Sid": "AllowECSFromLamda",
-      "Effect": "Allow",
-      "Action": [
-        "ecs:ListClusters",
-        "ecs:UpdateContainerAgent",
-        "ecs:DescribeClusters",
-        "ecs:DescribeContainerInstances",
-        "ecs:ListContainerInstances"
-      ],
-      "Resource": [
-        "*"
-      ]
-    }
-  ]
-}
-EOF
+  name   = "${local.environment}_${local.application_name}_lambda_policy"
+  role   = "${aws_iam_role.iam_for_lambda.id}"
+  policy = "${file("${path.module}/policies/lambda_kms_policy.json")}"
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
